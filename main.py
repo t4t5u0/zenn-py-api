a8a15c433452e6ee52154d6394fa00b3f68060b1
t4t5u0 <ymmtryk0902@gmail.com>
2022-02-28 02:47:14 +0900

    Update: 型をQiitaに合わせた

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import json
+from pprint import pp, pprint
 
 import requests
 
 from bs4 import BeautifulSoup
 
-with open('./secrets.json') as f:
-    load_json = json.load(f)
-    AUTHOR = load_json['author']
 
-ARTICLES = f"http://zenn.dev/{AUTHOR}"
+def main() -> dict:
 
-BOOKS = ARTICLES + '?tab=books'
+    with open("./secrets.json") as f:
+        load_json = json.load(f)
+        AUTHOR = load_json["author"]
+
+    ARTICLES = f"https://zenn.dev/{AUTHOR}"
+
+    BOOKS = ARTICLES + "?tab=books"
+
+    r_article = requests.get(ARTICLES)
+
+    soup = BeautifulSoup(r_article.text, "lxml")
+    title_text = soup.find("title").get_text()
+    print(title_text)
+    print("-" * len(title_text))
+
+    article_card = soup.select("article.ArticleCard_container__3qUYt")
+
+    return [
+        {
+            "title": article.select_one("h3.ArticleCard_title__UnBHE").get_text(),
+            "url": ARTICLES
+            + article.select_one("a.ArticleCard_mainLink__X2TOE").get("href"),
+            "tags": [
+                {"name": tag.get_text()}
+                for tag in article.select("a.ArticleCard_topicLink__NfdwJ")
+            ],
+            "likes_count ": int(
+                article.select_one("span.ArticleCard_like__lvRrs").get_text()
+            ),
+            "created_at": article.select_one("time").get("datetime"),
+        }
+        for article in article_card
+    ]
 
 
-r_article = requests.get(ARTICLES)
+if __name__ == "__main__":
+    result = main()
 
-soup = BeautifulSoup(r_article.text, 'lxml')
-title_text = soup.find('title').get_text()
-print(title_text)
+    from pprint import pprint
+
+    pprint(result)
