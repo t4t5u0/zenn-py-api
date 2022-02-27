ef07134292554778ccc1c6b71304b5e9c223398d
t4t5u0 <ymmtryk0902@gmail.com>
2022-02-28 01:55:32 +0900

    Update: titleにアクセス

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import json
+
 import requests
 
+from bs4 import BeautifulSoup
+
+with open('./secrets.json') as f:
+    load_json = json.load(f)
+    AUTHOR = load_json['author']
+
+ARTICLES = f"http://zenn.dev/{AUTHOR}"
+
+BOOKS = ARTICLES + '?tab=books'
+
+
+r_article = requests.get(ARTICLES)
+
+soup = BeautifulSoup(r_article.text, 'lxml')
+title_text = soup.find('title').get_text()
+print(title_text)
